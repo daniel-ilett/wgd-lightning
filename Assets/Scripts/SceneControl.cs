dc7227023b9c94a2d7b127d8f12ce6e2b8d74930
daniel-ilett <danielilett@gmail.com>
2017-11-03 23:20:45 +0000

    WGD Lightning Talk edition.

---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+	[SerializeField]
+	private string previousScene;
+
+	[SerializeField]
+	private string nextScene;
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("Next"))
+			SceneManager.LoadScene(nextScene);
+		else if (Input.GetButtonDown("Previous"))
+			SceneManager.LoadScene(previousScene);
+	}
+
+}
---
+++ Assets/Scripts/ToggleComponent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleComponent : MonoBehaviour
+{
+	[SerializeField]
+	private EffectType type;
+
+	private bool active = false;
+
+	private void Update()
+	{
+		if(Input.GetButtonDown("Toggle"))
+		{
+			active = !active;
+			switch (type)
+			{
+				case EffectType.Greyscale:
+					GetComponent<PPGreyscale>().enabled = active;
+					break;
+				case EffectType.Sepia:
+					GetComponent<PPSepia>().enabled = active;
+					break;
+				case EffectType.Blur:
+					GetComponent<PPGaussianBlur>().enabled = active;
+					break;
+				case EffectType.Depth:
+					GetComponent<PPDepth>().enabled = active;
+					break;
+				case EffectType.Edge:
+					GetComponent<PPSobelEdge>().enabled = active;
+					break;
+			}
+		}
+	}
+}
+
+enum EffectType
+{
+	Greyscale, Sepia, Blur, Depth, Edge, Cel
+}
+
